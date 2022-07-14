7fc91bb74cef726c4d362e7cb4d70cec8d3fab37
KNT <knt@gmail.com>
2022-07-14 11:19:37 +0700

    upload zens test algorithm

---
+++ Algorithm.js
@@ -0,0 +1,33 @@
+
+let array = [1, 3, 5, 7, 9];
+
+//Sort the array
+array.sort((firstNum, secoundNum) => {
+    if(secoundNum > firstNum){
+        return -1;
+    }
+    else {
+        return 0;
+    }
+});
+//Nếu hàm trả về số > 0 thì secoundNum sẽ đứng trước firstNum và ngược lại.
+
+//Sum the minium values
+function sumMinArray(array){
+    let sum = 0;
+    for (let i = 0; i < array.length -1; i++){
+        sum += array[i];
+    }
+    return sum;
+}
+
+//Sum the maxium values
+function sumMaxArray(array){
+    array.reverse();
+    let sum = 0;
+    for (let i = 0; i < array.length -1; i++){
+        sum += array[i];
+    }
+    return sum;
+}
+console.log(sumMinArray(array)+" "+sumMaxArray(array));
