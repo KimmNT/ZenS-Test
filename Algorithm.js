e82a1a1e25a3bd5cffaf7864b132229aa1f5f72e
KNT <knt@gmail.com>
2022-07-19 13:38:53 +0700

    upload algorithm test

--- Algorithm.js
+++ Algorithm.js
@@ -1,32 +1,30 @@
-
-let array = [1, 3, 5, 7, 9];
+let array = [14, 12, 51, 61, 2];
 
 //Sort the array
 array.sort((firstNum, secoundNum) => {
-    if(secoundNum > firstNum){
-        return -1;
-    }
-    else {
-        return 0;
-    }
+  if (secoundNum > firstNum) {
+    return -1;
+  } else {
+    return 0;
+  }
 });
 
-//Sum the minium values
-function sumMinArray(array){
-    let sum = 0;
-    for (let i = 0; i < array.length -1; i++){
-        sum += array[i];
-    }
-    return sum;
+//Sum the minium value
+function sumMinArray(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length - 1; i++) {
+    sum += array[i];
+  }
+  return sum;
 }
 
-//Sum the maxium values
-function sumMaxArray(array){
-    array.reverse();
-    let sum = 0;
-    for (let i = 0; i < array.length -1; i++){
-        sum += array[i];
-    }
-    return sum;
+// Sum the maxium value
+function sumMaxArray(array) {
+  array.reverse();
+  let sum = 0;
+  for (let i = 0; i < array.length - 1; i++) {
+    sum += array[i];
+  }
+  return sum;
 }
-console.log(sumMinArray(array)+" "+sumMaxArray(array));
+console.log(sumMinArray(array) + " " + sumMaxArray(array));
